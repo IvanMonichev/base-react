8cc7b4c7d6c51bc81be73db6f323dca85b29f0fa
Ivan Monichev <ivan@monichev.ru>
2023-02-09 01:24:00 +0300

    feat: add animation

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
 import PostItem from './PostItem';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const PostList = ({ posts, title, remove }) => {
 
   if (!posts.length) {
     return (
-      <h2 style={{textAlign: 'center'}}>Посты не были найдены.</h2>
+      <h2 style={{ textAlign: 'center' }}>Посты не были найдены.</h2>
     )
-  };
+  }
+  ;
 
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>{title}</h1>
-      {posts.map(((post, index) =>
-        <PostItem post={post} number={index + 1} key={post.id} remove={remove} />))}
+      <TransitionGroup>
+        {posts.map(((post, index) =>
+          <CSSTransition key={post.id} timeout={500} classNames="post">
+            <PostItem post={post} number={index + 1} remove={remove} />
+          </CSSTransition>))}
+      </TransitionGroup>
+
     </div>
-)
-  ;
+  );
 };
 
 export default PostList;
